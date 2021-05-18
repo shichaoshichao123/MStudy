150dfc67b55dfb3f2e2b34827fa2d21df6e5e8ea
yingqi <zdwh@MacBook-Pro.local>
2021-05-18 17:09:27 +0800

    1

---
+++ src/main/java/com/sc/study/offer/No03.java
@@ -0,0 +1,33 @@
+package com.sc.study.offer;
+
+/**
+ * @author yingqi
+ * @version 1.0
+ * @date 2021/5/18 5:05 下午
+ * @desc
+ *
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ */
+public class No03 {
+
+    /**
+     * 示例 1：
+     *
+     * 输入：
+     * [2, 3, 1, 0, 2, 5, 3]
+     * 输出：2 或 3
+     *
+     *
+     * 限制：
+     *
+     * 2 <= n <= 100000
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber(int[] nums) {
+
+    }
+
+
+
+}
