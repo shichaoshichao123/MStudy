15a13cb1fa23df8cd529f7d679b407686cad7b2d
yingqi <zdwh@MacBook-Pro.local>
2021-06-15 11:48:49 +0800

    2

--- src/main/java/com/sc/study/socket/Server.java
+++ src/main/java/com/sc/study/socket/Server.java
@@ -2,6 +2,7 @@ package com.sc.study.socket;
 
 import lombok.SneakyThrows;
 
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -14,8 +15,9 @@ public class Server {
 
     @SneakyThrows
     public static void main(String[] args) {
-
-
+        ServerSocket server = new ServerSocket(8888);
+        //打开服务
+        Socket socket = server.accept();
     }
 
     /**
